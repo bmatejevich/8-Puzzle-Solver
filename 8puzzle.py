472026999633b1787c164cfdc77112529074c8a7
Brian Matejevich <brianmatejevich@Brians-MacBook-Pro-7.local>
2020-06-25 11:22:10 -0400

    New README.md and cleaned up code

--- 8puzzle.py
+++ 8puzzle.py
@@ -1,26 +1,21 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# Welcome to the 8-puzzle solver! Below are functions that assist in the brute force search (BFS) of the 8-puzzle.
-
-# The BlankTileLocation function will search the current node for the position of the black tile and return it in rol col format.
-
-# In[1]:
-
+"""Welcome to the 8-puzzle solver! Below are functions that assist in the
+brute force search (BFS) of the 8-puzzle."""
+import numpy as np
 
 def BlankTileLocation(state):
+    """searches the puzzle for the location of the empyt space"""
     for row in range(len(state)):
         for col in range(len(state)):
             if state[row][col]==0:
                 return row,col
 
 
-# The AddNode function will add a "new" node to the nodeList if the node has not been seen before. If the nodes has been seen OR the node is not possible (move blank tile outside of puzzle) the node will not be added to the list.
-
-# In[2]:
-
 
 def AddNode(nodes,newNode):
+    """if a move is possible and not a repeated state it is added to the nodes list"""
     inSet = False
     for node in nodes:
         node1 = node[0][0]+node[0][1]+node[0][2]
@@ -37,12 +32,9 @@ def AddNode(nodes,newNode):
         nodes.append(newNode)
 
 
-# The below functions will move the blank tile in the given direction. If the move is not possible the function creates a faulty node which is recognized in the above function.
-
-# In[3]:
-
 
 def ActionMoveLeft(node):
+    """moves empty space to the left if possible, else returns fake node"""
     state = node
     row,col = BlankTileLocation(state[0])
     if col == 0:
@@ -56,6 +48,7 @@ def ActionMoveLeft(node):
     return status,newNode
 
 def ActionMoveRight(node):
+    """moves empty space to the right if possible, else returns fake node"""
     state = node
     row,col = BlankTileLocation(state[0])
     if col == 2:
@@ -69,6 +62,7 @@ def ActionMoveRight(node):
     return status,newNode
 
 def ActionMoveUp(node):
+    """moves empty space to the up if possible, else returns fake node"""
     state = node
     row,col = BlankTileLocation(state[0])
     if row == 0:
@@ -82,6 +76,7 @@ def ActionMoveUp(node):
     return status,newNode
 
 def ActionMoveDown(node):
+    """moves empty space to the down if possible, else returns fake node"""
     state = node
     row,col = BlankTileLocation(state[0])
     if row == 2:
@@ -95,29 +90,26 @@ def ActionMoveDown(node):
     return status,newNode
 
 
-# The below function "copyNode" was created because for some reason I was unable to find a way to pass the node by value and any changes I made to the node in the "ActionMove----" affected the original node.
-
-# In[4]:
 
 
 def copyNode(node):
+    """returns a copy of the node"""
     copy = [[0,0,0],[0,0,0],[0,0,0]]
     for y in range(3):
         for x in range(3):
             copy[y][x] = node[0][y][x]
     return [copy,node[1],node[2]]
 
-
-# The checkColvable function checks whether the given start node is solvable. It does this by counting the number of iversions, or times a smaller number comes after a larger number when the node is in a list format. EX: 1,2,3,4,5,6,8,7,0 this function has 1 inversion. The 0 term is considered a "9".
-
-# In[5]:
-
-
 def checkSolvable(node):
+    """The checkColvable function checks whether the given start node is solvable.
+     It does this by counting the number of iversions, or times a smaller number
+     comes after a larger number when the node is in a list format.
+     EX: 1,2,3,4,5,6,8,7,0 this function has 1 inversion. The 0 term is
+     considered a "9"."""
     list = []
     for row in range(3):
         for col in range(3):
-            list.append(node[row][col])       
+            list.append(node[row][col])
     inversions = 0
     for first in range(9):
         for second in range(8):
@@ -130,19 +122,9 @@ def checkSolvable(node):
         return True;
 
 
-# Below is my solver. It begins with the initial state of the system and an empty list of nodes. The goal state is defined and the start node is added to the nodelist. A counter to keep track of the current parent node is created along with a boolean variable to tell whether we have reached the goal state or not.
-
-# In[16]:
-
-
 def solve(startNode):
+    """ solve function takes in the initial state and will solve the puzzle if possible """
     nodes = []
-    
-    #this node will provide unsolvable
-    #startNode = [[[8,1,2],
-                  #[0,4,3],
-                  #[7,6,5]]   ,1,0]
-
     goal = [[1,2,3],[4,5,6],[7,8,0]]
     nodes.append(startNode)
     finished = False
@@ -152,8 +134,6 @@ def solve(startNode):
         finished = True
         print("unsolvable!")
     while not finished:
-
-
         copy = copyNode(nodes[i])
         status,newNode = ActionMoveDown(copy)
         newNode[1] = len(nodes)+1
@@ -180,7 +160,7 @@ def solve(startNode):
         AddNode(nodes,newNode)
 
         if newNode[0] == goal:
-            finished=True  
+            finished=True
             print("Solved!")
             finalNode = newNode
 
@@ -190,34 +170,16 @@ def solve(startNode):
         AddNode(nodes,newNode)
 
         if newNode[0] == goal:
-            finished=True 
+            finished=True
             print("Solved!")
             finalNode = newNode
 
         i +=1
-    
-    #print()
-    #x = 1   
-    #for node in nodes:
-     #       print("number: "+ str(x))
-      #      print(node[0][0])
-       #     print(node[0][1])
-        #    print(node[0][2])
-         #   print()
-          #  x +=1
-
-    #print("Steps: "+str(len(nodes)))
-    
     return finalNode,nodes
-    
-
-
-# The generatePath function creates a list using the format of [current index,parent index]. This list outlines the steps to complete the puzzle, it creates this list via backtracking.
-
-# In[14]:
 
 
 def generatePath(finalNode,nodes):
+    """ uses backtracking to find the path from start to goal state"""
     currentNode = finalNode
     parent_node_index = -1
     path = []
@@ -231,64 +193,43 @@ def generatePath(finalNode,nodes):
     return path
 
 
-# The below function takes the path and writes it to NodePath.txt for later use.
-
-# In[22]:
-
-
 def createNodePath(path,nodes):
-    file1 = open("NodePath.txt","w") 
+    """ writes the path returned by generate path to a txt file for later use"""
+    file1 = open("NodePath.txt","w")
     for node in path:
         currentNode = nodes[node[0]-1][0]
         for row in range(3):
             for col in range(3):
-                #print(currentNode[col][row],end=" ")
                 file1.write(str(currentNode[col][row])+" ")
                 if row ==2 and col == 2:
                     file1.write("\n")
-        #print()
-    file1.close() 
-
-
-# The below function creates the nodesInfo.txt file which follows the format of our generated path.
-
-# In[23]:
+    file1.close()
 
 
 def createNodesInfo(path):
-    file1 = open("NodesInfo.txt","w") 
+    """saves node path information to a txt file """
+    file1 = open("NodesInfo.txt","w")
     for node in path:
         file1.write(str(node[0])+" " + str(node[1])+ "\n")
         #print(node[0],node[1])
-    file1.close() 
-
-
-# The below function creates a Nodes.txt file which documents all of the explored nodes in the system by our solver.
-
-# In[25]:
-
+    file1.close()
 
 def createNodes(path,nodes):
-    file1 = open("Nodes.txt","w") 
+    """saves a list of all explored nodes """
+    file1 = open("Nodes.txt","w")
     count = 1
     for node in nodes:
         currentNode = node[0]
         for row in range(3):
             for col in range(3):
                 file1.write(str(currentNode[col][row])+" ")
-                #print(currentNode[col][row],end=" ")
         file1.write("\n")
-        #print()
         count+=1
-    file1.close() 
-
-
-# The below function was copied from the proffesor's supplied plot_path.py file to help visulize the solution.
-
-# In[11]:
+    file1.close()
 
 
 def print_matrix(state):
+    """function used to print solution step by step """
     counter = 0
     for row in range(0, len(state), 3):
         if counter == 0 :
@@ -299,9 +240,6 @@ def print_matrix(state):
             print(int(state[element]), "|", end=" ")
         counter = counter +1
         print("\n-------------")
-
-
-# In[26]:
 
 
 def main():
@@ -328,18 +266,14 @@ def main():
             print()
 
 
-# In[21]:
 
-#change start node to any configuration to attempt a solve.
-import numpy as np
+
 startNode = [[[1,2,3],
               [4,6,0],
               [7,5,8]]   ,1,0]
+
+"""this node will provide unsolvable"""
+#startNode = [[[8,1,2],
+              #[0,4,3],
+              #[7,6,5]]   ,1,0]
 main()
-
-
-# In[ ]:
-
-
-
-
--- README.md
+++ README.md
@@ -1,9 +1,3 @@
 # ENPM661project1 8-Puzzle Solver
 
-To run my 8-puzzle solver simply run the "8puzzle.py" file from the terminal by "python3 8puzzle.py" . If you would like to change the initial state, the startNode varible is at the bottom of the file before calling the main function. 
-
-The only library used was the numpy library which was used in the professor's code to visualize the solution.
-
-All comments are in text cells preceding the described code.(then exported from jupyter notebook to py file)
-
-All code was completed before the 2/13 class where I asked about the purpose of frequent commits. The only new code since then is the solvability function which is documented and based off the links on canvas. EDIT: new comparison of ints in the addnode function was added after class feb20.
+To run my 8-puzzle solver simply run the "8puzzle.py" file from the terminal by "python3 8puzzle.py" . If you would like to change the initial state, the startNode variable is at the bottom of the file.
